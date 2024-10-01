ee4b40fabc5885460c4533ff7271605edafc0047
vantage-ola <vantageola4@gmail.com>
2024-10-01 15:14:09 +0100

    chore: improve linter score, code improvements

--- app.py
+++ app.py
@@ -48,41 +48,37 @@ def user():
     '''
     Handles User information
     '''
-    # retrieve user's information.
     if request.method == 'GET':
         return jsonify([user.__dict__ for user in data['user']]), 200
-    
+    # retrieve user's information.
     body = request.get_json()
-    
     name = body['name']
     phone_number = body['phone_number']
     email = body['email_address']
 
     # store the new user information.
     if request.method == 'POST':
-
         if not check_phone_number(phone_number):
             return jsonify({"error": "Incorrect phone number !"}), 400
-        
+
         new_user = User(name, phone_number, email)
         data['user'].append(new_user)
-
         return jsonify(new_user.__dict__), 201
-    
+
     # edit the user information.
     if request.method == 'PUT':
 
-        for i, user in enumerate(data['user']): # find the user in the data.
+        for i, user in enumerate(data['user']):  # find the user in the data.
             if user.email_address == email:
-
                 if not check_phone_number(phone_number):
                     return jsonify({"error": "Incorrect phone number !"}), 400
-                
-                data['user'][i] = User(name, phone_number, email)  # update the user's info.
 
+                data['user'][i] = User(name, phone_number, email) # update the user's info.
                 return jsonify(data['user'][i].__dict__), 200
-            
+
         return jsonify({"error": "User not found !"}), 404
+    # add a default return statement for unsupported request methods
+    return jsonify({"error": "Unsupported request method !"}), 405
 
 @app.route('/resume/experience', methods=['GET', 'POST'])
 def experience():
