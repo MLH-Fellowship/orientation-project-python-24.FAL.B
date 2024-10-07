48e57445190f72513e9c728dbe4250a9e84f0de7
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2024-10-07 13:09:10 +0100

    feat: reorder section items on the backend

--- app.py
+++ app.py
@@ -80,42 +80,87 @@ def user():
     # add a default return statement for unsupported request methods
     return jsonify({"error": "Unsupported request method !"}), 405
 
-@app.route('/resume/experience', methods=['GET', 'POST'])
+@app.route('/resume/experience', methods=['GET', 'POST', 'PUT'])
 def experience():
     '''
     Handle experience requests
     '''
     if request.method == 'GET':
-        return jsonify()
+        return jsonify([item.__dict__ for item in data['experience']]), 200
 
     if request.method == 'POST':
         return jsonify({})
+    
+    if request.method == 'PUT':
+        body = request.get_json()
+        new_experience_order = []
+        for exp in body['data']:
+            title = exp['title']
+            company = exp['company']
+            start_date = exp['start_date']
+            end_date = exp['end_date']
+            description = exp['description']
+            logo = exp['logo']
 
-    return jsonify({})
+            new_experience_order.append(Experience(title, company, start_date, end_date, description, logo))
+        data['experience'] = new_experience_order
 
-@app.route('/resume/education', methods=['GET', 'POST'])
+        return_data = [item.__dict__ for item in data['experience']]
+        return jsonify(return_data), 200
+
+    return jsonify({"error": "Unsupported request method !"}), 405
+
+@app.route('/resume/education', methods=['GET', 'POST', 'PUT'])
 def education():
     '''
     Handles education requests
     '''
     if request.method == 'GET':
-        return jsonify({})
+        return jsonify([item.__dict__ for item in data['education']]), 200
 
     if request.method == 'POST':
         return jsonify({})
+    
+    if request.method == 'PUT':
+        body = request.get_json()
+        new_education_order = []
+        for edu in body['data']:
+            course = edu['course']
+            school = edu['school']
+            start_date = edu['start_date']
+            end_date = edu['end_date']
+            grade = edu['grade']
+            logo = edu['logo']
+            new_education_order.append(Education(course, school, start_date, end_date, grade, logo))
+        data['education'] = new_education_order
 
+        return_data = [item.__dict__ for item in data['education']]
+        return jsonify(return_data), 200
     return jsonify({})
 
 
-@app.route('/resume/skill', methods=['GET', 'POST'])
+@app.route('/resume/skill', methods=['GET', 'POST', 'PUT'])
 def skill():
     '''
     Handles Skill requests
     '''
     if request.method == 'GET':
-        return jsonify({})
+        return jsonify([item.__dict__ for item in data['skill']]), 200
 
     if request.method == 'POST':
         return jsonify({})
+    
+    if request.method == 'PUT':
+        body = request.get_json()
+        new_skill_order = []
+        for skill in body['data']:
+            name = skill['name']
+            proficiency = skill['proficiency']
+            logo = skill['logo']
+            new_skill_order.append(Skill(name, proficiency, logo))
+        data['skill'] = new_skill_order
+
+        return_data = [item.__dict__ for item in data['skill']]
+        return jsonify(return_data), 200
 
     return jsonify({})
