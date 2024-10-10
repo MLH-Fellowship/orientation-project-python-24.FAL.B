242b9e58388683dc6021df5c98c50cebf173ae31
C. <cairohyfr@gmail.com>
2024-10-09 20:39:34 -0500

    fix: resolve lint errors part 2

--- app.py
+++ app.py
@@ -3,35 +3,31 @@ Flask Application
 """
 
 from flask import Flask, jsonify, request
-from models import Experience, Education, Skill,User
-from utils import get_suggestion,check_phone_number,correct_spelling
-
+from models import Experience, Education, Skill, User
+from utils import get_suggestion, check_phone_number, correct_spelling
 
 
 app = Flask(__name__)
 
 data = {
-    "user": [
-        User("Jackie Stewart",
-             "+4478322678",
-             "jack@resume.com")
-    ],
+    "user": [User("Jackie Stewart", "+4478322678", "jack@resume.com")],
     "experience": [
-
-
-        Experience("Software Developer",
-                   "A Cool Company",
-                   "October 2022",
-                   "Present",
-                   "Writing Python Code",
-                   "example-logo.png"),
-        Experience("Intern",
-                   "A Nice Company",
-                   "October 2021",
-                   "December 2021",
-                   "Writing Scripts",
-                   "example-logo.png")
-
+        Experience(
+            "Software Developer",
+            "A Cool Company",
+            "October 2022",
+            "Present",
+            "Writing Python Code",
+            "example-logo.png",
+        ),
+        Experience(
+            "Intern",
+            "A Nice Company",
+            "October 2021",
+            "December 2021",
+            "Writing Scripts",
+            "example-logo.png",
+        ),
     ],
     "education": [
         Education(
@@ -54,62 +50,63 @@ def hello_world():
     """
     return jsonify({"message": "Hello, World!"})
 
-@app.route('/resume/user', methods=['GET', 'POST', 'PUT'])
+
+@app.route("/resume/user", methods=["GET", "POST", "PUT"])
 def user():
-    '''
+    """
     Handles User information
-    '''
+    """
+
     # defining sub function to reduce number of returns
     def get_users():
-        return jsonify([user.__dict__ for user in data['user']]), 200
+        return jsonify([user.__dict__ for user in data["user"]]), 200
 
     def add_user(body):
         # retrieve user's information.
-        name = body['name']
-        phone_number = body['phone_number']
-        email = body['email_address']
+        name = body["name"]
+        phone_number = body["phone_number"]
+        email = body["email_address"]
         # store the new user information.
         if not check_phone_number(phone_number):
             return jsonify({"error": "Incorrect phone number !"}), 400
         new_user = User(name, phone_number, email)
-        data['user'].append(new_user)
+        data["user"].append(new_user)
         return jsonify(new_user.__dict__), 201
 
     # edit the user information.
     def edit_user(body):
-        name = body['name']
-        phone_number = body['phone_number']
-        email = body['email_address']
-        for i, user_ in enumerate(data['user']):
+        name = body["name"]
+        phone_number = body["phone_number"]
+        email = body["email_address"]
+        for i, user_ in enumerate(data["user"]):
             if user_.email_address == email:
                 if not check_phone_number(phone_number):
                     return jsonify({"error": "Incorrect phone number !"}), 400
-                data['user'][i] = User(name, phone_number, email)
-                return jsonify(data['user'][i].__dict__), 200
+                data["user"][i] = User(name, phone_number, email)
+                return jsonify(data["user"][i].__dict__), 200
         return jsonify({"error": "User not found !"}), 404
 
-    if request.method == 'GET':
+    if request.method == "GET":
         return get_users()
-    if request.method == 'POST':
+    if request.method == "POST":
         body = request.get_json()
         return add_user(body)
-    if request.method == 'PUT':
+    if request.method == "PUT":
         body = request.get_json()
         return edit_user(body)
     return jsonify({"error": "Unsupported request method !"}), 405
 
+
 @app.route("/resume/experience", methods=["GET", "POST"])
 def experience():
-    """ 
-     Handle experience requests
     """
-   
+    Handle experience requests
+    """
 
-   
-    if request.method == 'GET':
+    if request.method == "GET":
         return jsonify({"experience": [exp.__dict__ for exp in data["experience"]]})
 
-    if request.method == 'POST':
+    if request.method == "POST":
         new_experience = request.json
         experience_instance = Experience(
             new_experience["title"],
@@ -117,11 +114,10 @@ def experience():
             new_experience["start_date"],
             new_experience["end_date"],
             new_experience["description"],
-            new_experience["logo"]
+            new_experience["logo"],
         )
         data["experience"].append(experience_instance)
         return jsonify({"id": len(data["experience"]) - 1})
-
 
     return jsonify({})
 
@@ -131,10 +127,10 @@ def education():
     """
     Handles education requests
     """
-    if request.method == 'GET':
+    if request.method == "GET":
         return jsonify({"education": [edu.__dict__ for edu in data["education"]]})
 
-    if request.method == 'POST':
+    if request.method == "POST":
         new_education = request.json
         education_instance = Education(
             new_education["course"],
@@ -142,11 +138,10 @@ def education():
             new_education["start_date"],
             new_education["end_date"],
             new_education["grade"],
-            new_education["logo"]
+            new_education["logo"],
         )
         data["education"].append(education_instance)
         return jsonify({"id": len(data["education"]) - 1})
-
 
     return jsonify({})
 
@@ -155,33 +150,37 @@ def education():
 def skill():
     """
     Handles Skill requests
-   """
+    """
 
-    if request.method == 'GET':
+    if request.method == "GET":
         return jsonify({"skills": [skill.__dict__ for skill in data["skill"]]})
 
-    if request.method == 'POST':
+    if request.method == "POST":
         new_skill = request.json
-        skill_instance = Skill(new_skill["name"], new_skill["proficiency"], new_skill["logo"])
+        skill_instance = Skill(
+            new_skill["name"], new_skill["proficiency"], new_skill["logo"]
+        )
         data["skill"].append(skill_instance)
         return jsonify({"id": len(data["skill"]) - 1})
 
     return jsonify({})
 
-@app.route('/resume/spellcheck', methods=['POST'])
+
+@app.route("/resume/spellcheck", methods=["POST"])
 def spellcheck():
-    '''
+    """
     Corrects the spelling of a text
-    '''
+    """
     body = request.get_json()
     try:
-        text = body['text']
+        text = body["text"]
         corrected_text = correct_spelling(text)
 
         return jsonify({"before": text, "after": corrected_text}), 200
     except KeyError:
         return jsonify({"error": "Missing text parameter"}), 400
-      
+
+
 @app.route("/suggestion", methods=["POST"])
 def get_description_suggestion():
     """
@@ -193,4 +192,3 @@ def get_description_suggestion():
         return jsonify({"error": "Description and type are required"}), 400
     suggestion = get_suggestion(description, type)
     return jsonify({"suggestion": suggestion})
-
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
-
-'''
+"""
 Utility Methods File
-'''
+"""
+
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
@@ -20,9 +20,9 @@ def get_suggestion(description, type):
     """
     give suggestions for description section using gemini (free alternative to openai's chatgpt api)
     """
-    prompt=""
+    prompt = ""
     if type == "education":
-        prompt = f"Improve the following education experience description for resume: {description}" 
+        prompt = f"Improve the following education experience description for resume: {description}"
     elif type == "experience":
         prompt = f"Improve the following professional experience description for resume: {description}"
 
@@ -31,20 +31,19 @@ def get_suggestion(description, type):
     return response.text
 
 
-
 def check_phone_number(phone_number):
-    """ Checks if the phone number is valid and follows
-        the international country code
+    """Checks if the phone number is valid and follows
+    the international country code
     """
-    regex = re.compile(r'^\+\d{1,3}\d{1,14}$')
+    regex = re.compile(r"^\+\d{1,3}\d{1,14}$")
     return bool(regex.match(phone_number))
 
 
 def correct_spelling(text: str):
-    """ Corrects the spelling of a text"""
+    """Corrects the spelling of a text"""
 
     spell_checker = SpellChecker()
-    word_pattern = r'\w+|[^\w\s]'
+    word_pattern = r"\w+|[^\w\s]"
 
     misspelled = spell_checker.unknown(re.findall(word_pattern, text))
     corrected_text = text
@@ -55,4 +54,3 @@ def correct_spelling(text: str):
             corrected_text = corrected_text.replace(word, correction)
 
     return corrected_text
-
